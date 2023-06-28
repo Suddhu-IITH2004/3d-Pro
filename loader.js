f1e486fb7ad38c308f97f9d75970835c40b12835
Suddhodhan <suddhu040804@gmail.com>
2023-06-28 16:07:11 +0530

    Preloading

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 $(window).on('load', function() {
 	setTimeout(function() {
 	  $('#loading-window').fadeOut('slow');
-	}, 30000); // 30 seconds in milliseconds
+	}, 3000); // 30 seconds in milliseconds
   });
   
